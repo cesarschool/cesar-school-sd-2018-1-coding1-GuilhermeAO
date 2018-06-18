e7a5a6be75033ed48a174a898b4c0148a5720323
GuilhermeAO <38106184+GuilhermeAO@users.noreply.github.com>
2018-06-18 16:52:08 -0300

    Update fnrc.c

--- Q3/fnrc.c
+++ Q3/fnrc.c
@@ -10,21 +10,69 @@
  */
 char firstNonRepeatingChar (char sentence[], int length)
 {
-    printf("\nfirstNonRepeatingChar::START: %s, %d\n", s, length);
+    printf("\nfirstNonRepeatingChar::START: %s, %d\n", sentence, length);
     
     // TODO: YOUR CODE HERE!
     
-    printf("\nfirstNonRepeatingChar::END\n");
-    return -1;
-}
+    int varsta = 0; 
+    
+    int varcont = 0;
+    
+    int varle = 0; 
+    
+    int vardone = length -1;
+    
+    while (varle <= vardone) {
+    
+        varcont = 0;
+    
+        while (varcont <= vardone) {
+    
+            if( sentence[varcont]==sentence[varle] && varcont != varle){
+    
+                varsta = 0;
+    
+                break;
+    
+            }
+    
+            varsta = 1;
+    
+            varcont = varcont +1;
+    
+        }
 
-int main()
-{
+
+    
+        if ( varsta==1){
+    
+            printf("A primeira letra que se repete é ( %c )",sentence[varle]);
+    
+            printf("\nfirstNonRepeatingChar::END\n");
+    
+            return sentence[varle];
+    
+        }
+    
+        varle=varle +1;
+      
+    }
+    
+    printf("-1 (nenhuma letra se repete)");
+    
+    printf("\nfirstNonRepeatingChar::END\n");
+    
+    
+    return -1;
+    
+}
+int main(){
     printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: firstNonRepeatingChar ###\n");
     
     firstNonRepeatingChar("ovo", 3);
     firstNonRepeatingChar("cesar school", 12);
     firstNonRepeatingChar("sistemas digitais", 17);
+    firstNonRepeatingChar("arara", 5);
     
     return 0;
 }
